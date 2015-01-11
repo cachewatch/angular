03dc07b4c2636f86070a3c13ca215a1b1adc1e37
pol <Alejo Next>
2015-01-11 17:34:38 -0500

    update ok

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 var cacheWatch = angular.module('cachewatch', ['ng']);
 
-cacheWatch.service('cachewacth', ['$rootScope', function ($rootScope) {
-	var me = {};
+cacheWatch.service('cachewatch', ['$rootScope', function ($rootScope) {
 	var _getTopScope = function() {
 		return $rootScope;
 		//return angular.element(document).scope();
 	};
 
-	me.ready = function() {
+	$rootScope.ready = function() {
 		var $scope = _getTopScope();
 		$scope.status = 'ready';
 		if(!$scope.$$phase) $scope.$apply();
 	};
-	me.loading = function() {
+	$rootScope.loading = function() {
 		var $scope = _getTopScope();
 		$scope.status = 'loading';
 		if(!$scope.$$phase) $scope.$apply();
 	};
-	me.$on('$routeChangeStart', function() {
+	$rootScope.$on('$routeChangeStart', function() {
 		_getTopScope().loading();
 	});
 
-	me.watch =  function (cb) {
-		var $scope = _getTopScope();
-		$scope.$watch('status', cb);
-	};
-
-	return me;
+	return {
+    ready : $rootScope.ready,
+    loading : $rootScope.loading
+  };
 }]);
 
 cacheWatch.directive('body', ['cachewatch',function (cache){
 	return {
 		rrestrict  : 'E',
 		link : function (scope, element) {
-			var ele = angular.element(element);
-			cache.watch(function(is){
-				ele.addAttr('data-ready', is);
-			});
+		 var ele = angular.element(element);
+      ele.attr('data-ready', scope.status);
 		}
 	}
 }]);
